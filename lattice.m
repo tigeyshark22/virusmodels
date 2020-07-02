1f5613863b56e14b479fc60672d4351b3f2a4c47
Owen Yang <yang_o@yahoo.com>
2020-07-02 16:00:44 -0700

    Adding the basic framework for ensemble statistics, debugged and added graphs for lattice_conv (now lattice)

--- lattice.m
+++ lattice.m
@@ -1,44 +1,46 @@
 %SIRD model
 clear
-
 pkg load statistics
 
 size=50; %side length of square
 
 days=30;
 
-initial_x=6; %first position of infected
-initial_y=8;
+initial_x=floor(size*rand())+1; %first position of infected
+initial_y=floor(size*rand())+1;
 
-infection_rate=.05; %the rate that each additional neighbor multiplies the infection by
+infection_rate=.2; %the rate that each additional neighbor multiplies the infection by
 infection_radius=3; %how much taxicab distance away someone can be and still infect
 infection_factor=2; %chance goes down by a factor of this for every further distance
-death_chance=.005;
-recovery_chance=.07;
-r_infection_rate=0; %how much each recovered person counts by as a neighbor, should be a decimal
+death_chance=.01;
+recovery_chance=.05;
 long_connections=5; %how many "longer distance" connections can infect people
 
-lattice_si=zeros(size.+2.*infection_radius,size.+2.*infection_radius);
-lattice_rd=zeros(size.+2.*infection_radius,size.+2.*infection_radius);
-%0 is susceptible, the borders are just there to prevent errors
-lattice_si(initial_x.+infection_radius,initial_y.+infection_radius)=1; %1 is infected
+lattice_si=zeros(size,size);
+lattice_rd=zeros(size,size);
+%0 is susceptible
+lattice_si(initial_x,initial_y)=1; %1 is infected
 
-infection_matrix=zeros(2.*infection_radius.+1,2.*infection_radius.+1); %setting up infection mechanism
+%setting up infection mechanism
 for radius=1:infection_radius
   for x_offset=0:radius
-    infection_matrix(infection_radius.+1.+x_offset,infection_radius.+1.+radius.-x_offset)=1./infection_factor.^(radius.-1); 
-    infection_matrix(infection_radius.+1.-x_offset,infection_radius.+1.+radius.-x_offset)=1./infection_factor.^(radius.-1);
-    infection_matrix(infection_radius.+1.+x_offset,infection_radius.+1.-radius.+x_offset)=1./infection_factor.^(radius.-1);
-    infection_matrix(infection_radius.+1.-x_offset,infection_radius.+1.-radius.+x_offset)=1./infection_factor.^(radius.-1);    
+    infection_matrix(infection_radius+1+x_offset,infection_radius+1+radius-x_offset)=1/infection_factor^(radius-1); 
+    infection_matrix(infection_radius+1-x_offset,infection_radius+1+radius-x_offset)=1/infection_factor^(radius-1);
+    infection_matrix(infection_radius+1+x_offset,infection_radius+1-radius+x_offset)=1/infection_factor^(radius-1);
+    infection_matrix(infection_radius+1-x_offset,infection_radius+1-radius+x_offset)=1/infection_factor^(radius-1);    
   endfor
 endfor
 
 for j=1:days
-  i=rand(size+2.*infection_radius);
+  i=rand(size);
   lattice_si_temp=lattice_si;
-  infected_count=0;
+    
+  lattice_neighbors=infection_rate*conv2(lattice_si,infection_matrix,"same")-i;
+  lattice_si_temp=lattice_si | (lattice_neighbors>0);
+  lattice_rd=lattice_rd+3*(lattice_si.*(i>(1-death_chance))) + 2*(lattice_si.*(i<recovery_chance));
+  lattice_si=lattice_si_temp & not(lattice_rd);
   
-  coords=floor(random("uniform", infection_radius.+1, size.+infection_radius, [4, long_connections]));
+  coords=floor(random("uniform",1,size,[4, long_connections]));
   for connectioncount=1:long_connections
     if abs(coords(1,connectioncount)-coords(3,connectioncount))+abs(coords(2,connectioncount)-coords(4,connectioncount))>=infection_radius
       x=coords(1,connectioncount);
@@ -69,72 +71,31 @@ for j=1:days
       endfor
     endif
   endfor
-
-  for x=infection_radius.+1:size.+infection_radius
-    for y=infection_radius.+1:size.+infection_radius
-      if lattice_si(x,y)==0 && lattice_rd(x,y)==0 %basic infection mechanism
-        infected_neighbors=0;
-        infection_multiply=lattice_si(x.-infection_radius:x.+infection_radius,y.-infection_radius:y.+infection_radius);
-        for row=1:2.*infection_radius.+1
-          infected_neighbors+=infection_multiply(row,:)*infection_matrix(:,row);
-        endfor
-        
-        if i(x,y)<=infection_rate*infected_neighbors
-          lattice_si_temp(x,y)=1;
-        endif
-      endif
-      
-      if lattice_si(x,y)==1 %determines what happens to infected
-        infected_count+=1;
-        if i(x,y)>=1-death_chance
-          lattice_rd(x,y)=3; %3 is dead
-          lattice_si_temp(x,y)=0;
-        endif
-        
-        if i(x,y)<=recovery_chance
-          lattice_rd(x,y)=2; %2 is recovered
-          lattice_si_temp(x,y)=r_infection_rate;
-        endif
-      endif
-      
-    endfor
-  endfor
-  lattice_si=lattice_si_temp;
-  if infected_count==0
+  
+  end_s(j)=sum(sum((lattice_si+lattice_rd)==0));
+  end_i(j)=sum(sum((lattice_si+lattice_rd)==1));
+  end_r(j)=sum(sum((lattice_si+lattice_rd)==2));
+  end_d(j)=sum(sum((lattice_si+lattice_rd)==3));
+  
+  if any(any(lattice_si))==0
     break
   endif
 endfor
+
 %plot
-end_s=0;
-end_i=0;
-end_r=0;
-end_d=0;
+
+figure(1)
+clf;
+colormap ("default");
+x=1:size;
+y=1:size;
+contourf(x,y,lattice_si(x,y)+lattice_rd(x,y),0:4)
+title ({"Final lattice"});
+
 figure(2)
+x=1:days;
 hold on
-for x=infection_radius.+1:size.+infection_radius
-  for y=infection_radius.+1:size.+infection_radius
-    if lattice_rd(x,y)==2
-      plot(x, y, 'og')
-      end_r+=1;
-    endif
-    if lattice_rd(x,y)==3
-      plot(x, y, 'ok')
-      end_d+=1;
-    endif
-    if lattice_rd(x,y)==0
-      if lattice_si(x,y)==0
-        plot(x, y, 'ob')
-        end_s+=1;
-      endif
-      if lattice_si(x,y)==1
-        plot(x, y, 'or')
-        end_i+=1;
-      endif
-    endif
-  endfor
-endfor
-end_s
-end_i
-end_r
-end_d
- 
+plot(x,end_s(x),'b','LineWidth',1)
+plot(x,end_i(x),'r','LineWidth',1)
+plot(x,end_r(x),'g','LineWidth',1)
+plot(x,end_d(x),'k','LineWidth',1)
